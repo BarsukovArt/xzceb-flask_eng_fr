1626cc6a419a1e9c3c911ea28158c8f6d5420533
BarsukovArt <barsukov.art@gmail.com>
2023-01-25 14:17:43 -0500

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,40 @@
+"""
+Translator
+"""
+import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def englishToFrench(englishText):
+    """
+    From English to French
+    """
+    translation = language_translator.translate(
+        text=englishText,
+        model_id='en-fr').get_result()
+    return translation.get('translations')[0].get('translation')
+
+def frenchToEnglish(frenchText):
+    """
+    From French to English
+    """
+    translation = language_translator.translate(
+        text=frenchText,
+        model_id='fr-en').get_result()
+    return translation.get('translations')[0].get('translation')
